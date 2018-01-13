01cccf9830e7e5b3c0ae365fc0ebb3b9c53fe6a4
Vijay Vaidyanathan <vvijay26@gmail.com>
2018-01-13 11:01:13 -0800

    Spring jdbc, jpa and spring data using H2/mysql
    Work in progress

---
+++ demo-spring-database/src/main/java/com/vvijay26/springdata/demospringdatabase/DemoSpringDatabaseApplication.java
@@ -0,0 +1,12 @@
+package com.vvijay26.springdata.demospringdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringDatabaseApplication.class, args);
+	}
+}
---
+++ demo-spring-database/src/test/java/com/vvijay26/springdata/demospringdatabase/DemoSpringDatabaseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vvijay26.springdata.demospringdatabase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoSpringDatabaseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
